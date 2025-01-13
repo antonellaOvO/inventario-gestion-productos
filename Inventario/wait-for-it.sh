3ced5927caf5c93ec4a27481618913e5e0919f83
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2025-01-13 19:56:55 +0100

    modified script wait and dockerfile

--- Inventario/wait-for-it.sh
+++ Inventario/wait-for-it.sh
@@ -1,19 +1,12 @@
-#!/bin/bash
-# wait-for-it.sh: espera a que un servicio esté disponible (en este caso, MySQL)
+#!/usr/bin/env bash
 
-HOST=$1
-PORT=$2
-shift 2
+host="$1"
+port="$2"
 
-echo "Esperando que el servicio $HOST:$PORT esté disponible..."
-
-# Esperar hasta que MySQL esté listo
-until nc -z -v -w30 $HOST $PORT; do
-  echo "Esperando MySQL..."
-  sleep 2
+while ! echo > /dev/tcp/"$host"/"$port"; do
+  echo "Esperando MySQL en $host:$port..."
+  sleep 1
 done
 
-echo "Servicio $HOST:$PORT está disponible, continuando con la ejecución."
-
-# Ejecutar el comando que sigue (en este caso, iniciar la aplicación)
-exec "$@"
+echo "MySQL disponible, ejecutando la aplicación."
+exec "${@:3}"

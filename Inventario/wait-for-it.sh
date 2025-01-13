adcd42d8484633006fb48e3605511fe6d72d7e61
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2025-01-13 20:20:35 +0100

    modified dockerfile and wait script

--- Inventario/wait-for-it.sh
+++ Inventario/wait-for-it.sh
@@ -3,10 +3,12 @@
 host="$1"
 port="$2"
 
+echo "Comprobando conexión a $host en el puerto $port..."
+
 while ! echo > /dev/tcp/"$host"/"$port"; do
   echo "Esperando MySQL en $host:$port..."
   sleep 1
 done
 
-echo "MySQL disponible, ejecutando la aplicación."
+echo "MySQL disponible en $host:$port, ejecutando la aplicación."
 exec "${@:3}"
